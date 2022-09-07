5145c4d0796f2be8c9b7c8a1a5fbf6e003811e23
Rofson Jame Abu Siam <abu.siam610@gmail.com>
2022-09-07 22:26:51 +0600

    fix production bug in app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
 // production
-if (process.env.NODE_ENV){
+if (process.env.NODE_ENV==='production'){
     app.use(express.static(path.join(__dirname, '.clinet/build')))
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '.client/build'))
